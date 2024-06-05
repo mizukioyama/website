d4259e9a2dc77de63f45f5dd3fbdcbdd90f90e47
MizukiOyama <154662763+MizukiOyama@users.noreply.github.com.>
2024-06-05 17:50:20 +0900

    html

--- JS/menu.js
+++ JS/menu.js
@@ -1,103 +1,106 @@
-    // カーソル
-    $(function () {
-        var cursor = $("#cursor");
-        var stalker = $("#stalker");
+// menu.jsの内容をここに記述します
+console.log('Menu script loaded');
 
-        $("a").hover(
-            function () {
-                cursor.addClass('cursor--hover');
-            },
-            function () {
-                cursor.removeClass('cursor--hover');
-            }
-        );
+// カーソル
+$(function() {
+    var cursor = $("#cursor");
+    var stalker = $("#stalker");
 
-        $(document).on("mousemove", function (e) {
-            var x = e.clientX;
-            var y = e.clientY;
-            cursor.css({
+    $("a").hover(
+        function() {
+            cursor.addClass('cursor--hover');
+        },
+        function() {
+            cursor.removeClass('cursor--hover');
+        }
+    );
+
+    $(document).on("mousemove", function(e) {
+        var x = e.clientX;
+        var y = e.clientY;
+        cursor.css({
+            "opacity": "1",
+            "top": y + "px",
+            "left": x + "px"
+        });
+
+        setTimeout(function() {
+            stalker.css({
                 "opacity": "1",
                 "top": y + "px",
                 "left": x + "px"
             });
-
-            setTimeout(function () {
-                stalker.css({
-                    "opacity": "1",
-                    "top": y + "px",
-                    "left": x + "px"
-                });
-            }, 150);
-        });
+        }, 150);
     });
+});
 
-    // メニュー
-    jQuery(document).ready(function($) {
-        var $nav = $('#navArea');
-        var $btn = $('.toggle_btn');
-        var $mask = $('#mask');
-        var open = 'open';
+// メニュー
+jQuery(document).ready(function($) {
+    var $nav = $('#navArea');
+    var $btn = $('.toggle_btn');
+    var $mask = $('#mask');
+    var open = 'open';
 
-        $btn.on('click', function() {
-            if (!$nav.hasClass(open)) {
-                $nav.addClass(open);
-            } else {
-                $nav.removeClass(open);
-            }
-        });
-
-        $mask.on('click', function() {
+    $btn.on('click', function() {
+        if (!$nav.hasClass(open)) {
+            $nav.addClass(open);
+        } else {
             $nav.removeClass(open);
-        });
+        }
     });
 
-    //img
-    (function() {
-        // 遅延読み込みのためのスクリプト
-            document.addEventListener("DOMContentLoaded", function() {
-            var lazyImages = document.querySelectorAll('img[data-src]');
+    $mask.on('click', function() {
+        $nav.removeClass(open);
+    });
+});
 
-            lazyImages.forEach(function(img) {
-                img.setAttribute('src', img.getAttribute('data-src'));
-                img.onload = function() {
+// img
+(function() {
+    document.addEventListener("DOMContentLoaded", function() {
+        var lazyImages = document.querySelectorAll('img[data-src]');
+
+        lazyImages.forEach(function(img) {
+            img.setAttribute('src', img.getAttribute('data-src'));
+            img.onload = function() {
                 img.removeAttribute('data-src');
-                };
-            });
-            });
-        })();
-
-    // タイピング
-    $(window).on('load', function() {
-        $(".typing").each(function() {
-            var text = $(this).text();
-            var textbox = "";
-            var words = text.split(' ');
-            for (var i = 0; i < words.length; i++) {
-                var word = words[i];
-                for (var j = 0; j < word.length; j++) {
-                    textbox += '<span>' + word[j] + '</span>';
-                }
-                if (i < words.length - 1) {
-                    textbox += '<span>&nbsp;</span>';
-                }
-            }
-            $(this).html(textbox);
-
-            $(".typing span").each(function(i) {
-                var time = 100;
-                $(this).delay(time * i).fadeIn(time);
-            });
-
-            if (!$(this).hasClass('menu-animation-target')) {
-                var positionX = Math.random() * ($(this).parent().width() - $(this).width());
-                var positionY = Math.random() * ($(this).parent().height() - $(this).height());
-                $(this).css({ 'left': positionX, 'top': positionY });
-            }
-
-            if ($(this).hasClass('animation1')) {
-                $(this).addClass('animation1-style');
-            } else if ($(this).hasClass('animation2')) {
-                $(this).addClass('animation2-style');
-            }
+            };
         });
     });
+})();
+
+// タイピング
+$(window).on('load', function() {
+    $(".typing").each(function() {
+        var text = $(this).text();
+        var textbox = "";
+        var words = text.split(' ');
+
+        for (var i = 0; i < words.length; i++) {
+            var word = words[i];
+            for (var j = 0; j < word.length; j++) {
+                textbox += '<span>' + word[j] + '</span>';
+            }
+            if (i < words.length - 1) {
+                textbox += '<span>&nbsp;</span>';
+            }
+        }
+        $(this).html(textbox);
+
+        $(".typing span").each(function(i) {
+            var time = 100;
+            $(this).delay(time * i).fadeIn(time);
+        });
+
+        if (!$(this).hasClass('menu-animation-target')) {
+            var positionX = Math.random() * ($(this).parent().width() - $(this).width());
+            var positionY = Math.random() * ($(this).parent().height() - $(this).height());
+            $(this).css({ 'left': positionX, 'top': positionY });
+        }
+
+        if ($(this).hasClass('animation1')) {
+            $(this).addClass('animation1-style');
+        } else if ($(this).hasClass('animation2')) {
+            $(this).addClass('animation2-style');
+        }
+    });
+});
