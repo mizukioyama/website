e4c8656bb6d156c4b70ce3c81da2515107113470
MizukiOyama <154662763+MizukiOyama@users.noreply.github.com>
2024-06-12 18:44:05 +0900

    Update loading.js

--- JS/loading.js
+++ JS/loading.js
@@ -3,15 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('load', function() {
         setTimeout(function() {
             document.getElementById('loading-bg').style.display = 'none';
-            TypingAnimation();  // テキストのアニメーション効果を実行
         }, 2000);
+
+        // ページ読み込み時にテキストアニメーションを実行
+        TypingAnimation();
     });
 
     // テキストのアニメーション効果を実装する関数
     function TypingAnimation() {
         var elements = document.querySelectorAll('.js_typing');
         elements.forEach(function(element) {
-            element.classList.add('endAnime');
+            if (!element.classList.contains('endAnime')) {
+                element.classList.add('endAnime');
+            }
         });
     }
 });
