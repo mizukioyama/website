acda2f00bd04b78886a33f4b829009b93fc7d7ef
MizukiOyama <154662763+MizukiOyama@users.noreply.github.com>
2024-06-13 16:58:21 +0900

    Update cursor.js

--- JS/cursor.js
+++ JS/cursor.js
@@ -2,29 +2,39 @@ jQuery.noConflict();
 
 (function ($) {
     $(function () {
-        var stalker = $("#stalker");
+        var cursor = $("<div id='cursor'></div>");
+        var stalker = $("<div id='stalker'></div>");
+        $("body").addClass("custom-cursor").append(cursor).append(stalker);
 
         $("a").hover(
             function () {
+                cursor.addClass('cursor--hover');
                 stalker.addClass('stalker--hover');
             },
             function () {
+                cursor.removeClass('cursor--hover');
                 stalker.removeClass('stalker--hover');
             }
         );
 
-        stalker.css("opacity", "0");
-
         $(document).on("mousemove", function (e) {
             var x = e.clientX;
             var y = e.clientY;
+            cursor.css({
+                "opacity": "1",
+                "top": y + "px",
+                "left": x + "px"
+            });
             setTimeout(function () {
                 stalker.css({
                     "opacity": "1",
                     "top": y + "px",
                     "left": x + "px"
                 });
-            }, 180);
+            }, 150);
         });
+
+        cursor.css("opacity", "0");
+        stalker.css("opacity", "0");
     });
 })(jQuery);
