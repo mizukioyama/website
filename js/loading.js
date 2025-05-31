0b3f2af6d20b4d23c9eadd38144590aeea3f9755
m.oyama <154662763+PDmizuki@users.noreply.github.com>
2025-05-31 13:01:37 +0900

    ローディング

--- js/loading.js
+++ js/loading.js
@@ -1,68 +1,65 @@
 class ShuffleText {
-    constructor(element) {
-        this.element = element;
-        this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        this.originalText = element.textContent;
-    }
+  constructor(element) {
+    this.element = element;
+    this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    this.originalText = element.textContent;
+  }
 
-    setText(text) {
-        this.originalText = text;
-    }
+  setText(text) {
+    this.originalText = text;
+  }
 
-    randomChar() {
-        return this.chars[Math.floor(Math.random() * this.chars.length)];
-    }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
 
-    shuffle() {
-        let shuffledText = '';
-        for (let i = 0; i < this.originalText.length; i++) {
-            shuffledText += this.randomChar();
-        }
-        this.element.textContent = shuffledText;
+  shuffle() {
+    let shuffledText = '';
+    for (let i = 0; i < this.originalText.length; i++) {
+      shuffledText += this.randomChar();
     }
+    this.element.textContent = shuffledText;
+  }
 
-    start() {
-        let count = 0;
-        const maxCount = 20;
-        const interval = 100;
-        const shuffleInterval = setInterval(() => {
-            this.shuffle();
-            count++;
-            if (count >= maxCount) {
-                clearInterval(shuffleInterval);
-                this.element.textContent = this.originalText;
-            }
-        }, interval);
-    }
+  start() {
+    let count = 0;
+    const maxCount = 20;
+    const interval = 100;
+    const shuffleInterval = setInterval(() => {
+      this.shuffle();
+      count++;
+      if (count >= maxCount) {
+        clearInterval(shuffleInterval);
+        this.element.textContent = this.originalText;
+      }
+    }, interval);
+  }
 }
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
+  function TypingAnimation() {
+    $(".js_typing").each(function () {
+      const $el = $(this);
+      if (!$el.hasClass("endAnime")) {
+        const text = $el.text();
+        const shuffleText = new ShuffleText(this);
+        shuffleText.setText(text);
+        shuffleText.start();
 
-    function TypingAnimation() {
-        $(".js_typing").each(function () {
-            const $el = $(this);
-            if (!$el.hasClass("endAnime")) {
-                const text = $el.text();
-                $el.text('');
-                $el.addClass("endAnime"); // opacity: 1 にする
-                $el.css("opacity", 1);     // 念のため明示的に設定
-
-                const shuffleText = new ShuffleText(this);
-                shuffleText.setText(text);
-                shuffleText.start();
-            }
-        });
-    }
-
-    $(window).on('load', function () {
-        setTimeout(function () {
-            $('#loading-bg').fadeOut(300, function () {
-                TypingAnimation();
-            });
-        }, 1500);
+        $el.addClass("endAnime"); // flicker animationを停止
+      }
     });
+  }
 
-    $(window).scroll(function () {
+  $(window).on('load', function () {
+    setTimeout(function () {
+      $('#loading-bg').fadeOut(500, function () {
         TypingAnimation();
-    });
+      });
+    }, 1500);
+  });
+
+  $(window).scroll(function () {
+    TypingAnimation();
+  });
 });
