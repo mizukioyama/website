cc83b2a80549ddbdb8228706e1a0e3df88a254fa
m.oyama <154662763+PDmizuki@users.noreply.github.com>
2025-06-01 18:39:00 +0900

    load

--- js/loading.js
+++ js/loading.js
@@ -2,7 +2,7 @@ jQuery(function ($) {
   $(window).on('load', function () {
     $('html').css('display', 'block'); // ← 保険的に追加
     setTimeout(function () {
-      $('#loading-bg').fadeOut(500);
-    }, 2500);
+      $('#loading-bg').fadeOut(1200);
+    }, 3500);
   });
 });
