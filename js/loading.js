b325c388fd922b89d9385c0e2b5d431f99c269bf
m.oyama <154662763+mizukioyama@users.noreply.github.com>
2025-06-03 14:24:24 +0900

    load 言語 h1

--- js/loading.js
+++ js/loading.js
@@ -1,10 +1,10 @@
 jQuery(function ($) {
   $(window).on('load', function () {
     $('html').css('display', 'block');
-    
-    // タイピング完了後にフェードアウト (6s + 少し余裕をもたせて 6.5秒)
+
+    // 最後のアニメーション後に少し待ってフェードアウト
     setTimeout(function () {
       $('#loading-bg').fadeOut(800);
-    }, 6500);
+    }, 4500); // 3行目のアニメーション完了（3.1s）＋余裕
   });
 });
