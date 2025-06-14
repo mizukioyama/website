3f61956039d456a079e8653238d97a51b21a6251
m.oyama <154662763+mizukioyama@users.noreply.github.com>
2025-06-14 15:19:16 +0900

    lang

--- js/page-nation.js
+++ js/page-nation.js
@@ -282,6 +282,9 @@ function setupCategoryFilter() {
 document.getElementById("langChenge").addEventListener("click", () => {
   currentLang = currentLang === "ja" ? "en" : "ja";
   renderGallery(); // これで表示が再レンダリングされる
+
+// ボタン表示も切り替え
+  document.getElementById("langChenge").textContent = currentLang === "ja" ? "EN" : "JA";
 });
 
 
