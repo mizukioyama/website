68655ea149cc649d2dabbb6a317e8d1289aaae48
m.oyama <154662763+mizukioyama@users.noreply.github.com>
2025-06-18 17:27:50 +0900

    img

--- js/page-nation.js
+++ js/page-nation.js
@@ -7,168 +7,357 @@ function setupCategoryFilter() {
             title: { ja: "蒼想 / 2024", en: "Blue Thought / 2024" },
             caption: { ja: "Order：アナログ（ペイント）<br>", en: "Order: Analog (Paint)<br>" },
             category: ["analog"],
-            img: "img/2024works.jpg",
+            img: "img/蒼想01.jpg",
             link: "index.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：AI生成", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/20230629.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：幸緑 AI", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/21ai.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：挑戦 AI生成", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/22ai.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：加工 AI生成", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/230011-2.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：加工 AI生成", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/230029-2.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：写真", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/230033-2.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：挑戦 加工 / AI生成", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/230051-2.jpg",
-            link: "pages/2024work.html"
-        },
+        },//========69~60
         {
             title: { ja: "No Title / 2023", en: "No Title / 2023" },
             caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/230052-2.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "心樹 / 2023", en: "心樹 / 2023" },
-            caption: { ja: "心樹 / 2023<br>アナログ作品「挑戦」から芽吹いた、デジタルの樹。<br><br>幾重にも重ねた編集の葉が、やがてひとつの球体を結び、<br>それは心の奥底──静かにたゆたう源（みなもと）を象っています。<br>大地に根を張るように、青は深い静けさを、<br>緑は命の循環を、黄色は内に宿る希望の光を。<br>自然の色をまといながら、目には見えない「心の樹形図」を描きました。", en: "Unpublished: AI generated" },
-            category: ["digital", "analog"],
-            img: "img/230053-2.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：AI生成", en: "Unpublished: AI generated" },
-            category: ["digital", "analog"],
-            img: "img/230055-2.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：写真 / AI生成", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/230035-2.jpg",
-            link: "pages/2024work.html"
-        },
-        {
-            title: { ja: "No Title / 2023", en: "No Title / 2023" },
-            caption: { ja: "未発表作品：", en: "Unpublished: AI generated" },
             category: ["digital"],
-            img: "img/R0021433.jpg",
+            img: "img/202362-2.jpg",
             link: "pages/2024work.html"
         },
         {
             title: { ja: "No Title / 2023", en: "No Title / 2023" },
             caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
             category: ["digital"],
-            img: "img/white.jpg",
+            img: "img/202361-2.jpg",
             link: "pages/2024work.html"
         },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202360-2.jpg",
+            link: "pages/2024work.html"
+        },//========59~50
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202359-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202358-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202357-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202356-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202355-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202354-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202353-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202352-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202351-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202350-2.jpg",
+            link: "pages/2024work.html"
+        },//========49~40
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202349-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202348-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202347-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202346-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202345-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202344-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202343-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202342-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202341-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202340-2.jpg",
+            link: "pages/2024work.html"
+        },//========39~30
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202339-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202338-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202337-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202336-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202335-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202334-2.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "No Title / 2023", en: "No Title / 2023" },
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/202333-2.jpg",
+            link: "pages/2024work.html"
+        },//========2022
         {
             title: { ja: "No Title / 2022", en: "No Title / 2022" },
-            caption: { ja: "未発表作品：デジタル / Photo", en: "Unpublished: AI generated" },
-            category: ["digital", "ai"],
-            img: "img/wallart.jpg",
+            caption: { ja: "未発表作品：デジタル", en: "Unpublished: AI generated" },
+            category: ["digital"],
+            img: "img/2202200.jpg",
             link: "pages/2024work.html"
         },
         {
             title: { ja: "映し神 / 2022", en: "No Title / 2022" },
             caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
             category: ["analog"],
-            img: "img/映し神.jpg",
+            img: "img/映し神01.jpg",
             link: "pages/2024work.html"
         },
         {
             title: { ja: "挑戦 / 2022", en: "No Title / 2022" },
             caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
             category: ["analog"],
-            img: "img/挑戦.jpg",
+            img: "img/挑戦01.jpg",
             link: "pages/2024work.html"
-        },
+        },//========2021
         {
             title: { ja: "戦争 / 2021", en: "No Title / 2021" },
             caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
             category: ["analog"],
-            img: "img/戦争.jpg",
+            img: "img/戦争01.jpg",
             link: "pages/2024work.html"
         },
         {
             title: { ja: "幸緑 / 2021", en: "No Title / 2021" },
             caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
             category: ["analog"],
-            img: "img/幸緑.jpg",
+            img: "img/幸緑01.jpg",
             link: "pages/2024work.html"
         },
         {
             title: { ja: "金雲 / 2021", en: "No Title / 2021" },
             caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
             category: ["analog"],
-            img: "img/金雲.jpg",
+            img: "img/金雲01.jpg",
             link: "pages/2024work.html"
         },
         {
             title: { ja: "幸華 / 2021", en: "No Title / 2021" },
             caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
             category: ["analog"],
-            img: "img/幸華.jpg",
+            img: "img/幸華01.jpg",
             link: "pages/2024work.html"
         },
         {
             title: { ja: "輝き / 2021", en: "No Title / 2021" },
             caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
             category: ["analog"],
-            img: "img/輝き.jpg",
+            img: "img/輝き01.jpg",
+            link: "pages/2024work.html"
+        },//========2010~17
+        {
+            title: { ja: "海底 / 2017", en: "No Title / 2017" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/海底01.jpg",
             link: "pages/2024work.html"
         },
         {
             title: { ja: "無題 / 2017", en: "No Title / 2017" },
             caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
             category: ["analog"],
-            img: "img/10-1702.jpg",
+            img: "img/2010-1701.jpg",
             link: "pages/2024work.html"
         },
         {
-            title: { ja: "海底 / 2010", en: "No Title / 2010" },
+            title: { ja: "無題 / 2016", en: "No Title / 2016" },
             caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
             category: ["analog"],
-            img: "img/海底.jpg",
+            img: "img/2010-1702.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "無題 / 2016", en: "No Title / 2016" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/2010-1703.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "無題 / 2010", en: "No Title / 2010" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/2010-1704.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "無題 / 2010", en: "No Title / 2010" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/2010-1705.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "無題 / 2010", en: "No Title / 2010" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/2010-1706.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "無題 / 2010", en: "No Title / 2010" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/2010-1707.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "無題 / 2010", en: "No Title / 2010" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/2010-1708.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "無題 / 2010", en: "No Title / 2010" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/2010-1709.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "無題 / 2010", en: "No Title / 2010" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/2010-1710.jpg",
+            link: "pages/2024work.html"
+        },
+        {
+            title: { ja: "無題 / 2010", en: "No Title / 2010" },
+            caption: { ja: "展示：アナログ（ペイント）", en: "Unpublished: AI generated" },
+            category: ["analog"],
+            img: "img/2010-1711.jpg",
             link: "pages/2024work.html"
         }
     ];
