5ccfdc1e5f6d6013efdd50356146b5985169bd01
m.oyama <154662763+mizukioyama@users.noreply.github.com>
2025-06-19 13:57:32 +0900

    Cursor

--- js/cursor.js
+++ js/cursor.js
@@ -38,14 +38,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-  const cursor = document.createElement("div");
-  cursor.id = "cursor";
-  document.body.appendChild(cursor);
-
-  const stalker = document.createElement("div");
-  stalker.id = "stalker";
-  document.body.appendChild(stalker);
-
   // 追従処理
   document.addEventListener("mousemove", e => {
     const { clientX: x, clientY: y } = e;
@@ -62,7 +54,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 100);
   });
 
-  // 初期非表示（オプション）
-  cursor.style.opacity = "0";
-  stalker.style.opacity = "0";
+  // ホバー処理（対象を自由に追加可能）
+  document.querySelectorAll("body a, button, .button, label, header li a, footer a, #category-header, #category-menu li, #sidebar-container li, #gallery-container a, #pagination").forEach(el => {
+    el.addEventListener("mouseenter", () => {
+      cursor.classList.add("cursor--hover");
+      stalker.classList.add("stalker--hover");
+    });
+    el.addEventListener("mouseleave", () => {
+      cursor.classList.remove("cursor--hover");
+      stalker.classList.remove("stalker--hover");
+    });
+  });
+
 });
