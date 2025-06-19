1064bb3bf9cbcec0cb7428988bfbe272824c8c58
m.oyama <154662763+mizukioyama@users.noreply.github.com>
2025-06-19 13:52:54 +0900

    cu

--- js/cursor.js
+++ js/cursor.js
@@ -35,34 +35,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-
-document.addEventListener("DOMContentLoaded", function () {
-  const cursor = document.createElement("div");
-  cursor.id = "cursor";
-  document.body.appendChild(cursor);
-
-  const stalker = document.createElement("div");
-  stalker.id = "stalker";
-  document.body.appendChild(stalker);
-
-  // 追従処理
-  document.addEventListener("mousemove", e => {
-    const { clientX: x, clientY: y } = e;
-
-    cursor.style.opacity = "1";
-    stalker.style.opacity = "1";
-
-    cursor.style.top = `${y}px`;
-    cursor.style.left = `${x}px`;
-
-    setTimeout(() => {
-      stalker.style.top = `${y}px`;
-      stalker.style.left = `${x}px`;
-    }, 100);
-  });
-
-  // 初期非表示（オプション）
-  cursor.style.opacity = "0";
-  stalker.style.opacity = "0";
-});
